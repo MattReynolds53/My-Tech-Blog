5f97cb84b1affd047055c6425b88dc8f3dc6fa97
Matt Reynolds <mattreynolds53@gmail.com>
2022-01-13 08:48:14 -0600

    Continued to work on getting the comments to post. Made progress and am almost there, but there a still a couple of errors to fix with the routes and connections.

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -4,15 +4,14 @@ const withAuth = require('../../utils/auth');
 
 
 // There is a missing connection between the blogPost ID and the comment ID because the following console log shows that req.session.userId is undefined and the postId is showing up as an empty string in the console (from line 8-9). How can I fix that issue? The comments are showing up in the Workbench, but I'm not sure why they aren't apending to the blog post itself (I am assuming it is because of the aforementioned issue).
-router.post('/', withAuth, (req, res) => {
-  console.log('HELLO', req.body, req.session.userId);
-  Comment.create({ ...req.body, user_id: req.session.userId })
-    .then((newComment) => {
-      res.json(newComment);
-    })
-    .catch((err) => {
+router.post('/', withAuth, async (req, res) => {
+  console.log('HELLO', req.body, req.session);
+  try {const newComment = await Comment.create({ ...req.body, user_id: req.session.user_id })
+  res.status(200).json(newComment)
+} catch(err){
+    
       res.status(500).json(err);
-    });
+    };
 });
 
 module.exports = router;
--- public/js/comment.js
+++ public/js/comment.js
@@ -1,14 +1,17 @@
 const commentFormHandler = async function(event) {
     event.preventDefault();
+ 
   
-    const postId = document.querySelector('input[name="post-id"]').value;
+    //const postId = document.querySelector('input[name="post-id"]').value;
     const body = document.querySelector('textarea[name="comment-body"]').value;
+    //console.log(postId);
+    console.log(body);
   
     if (body) {
       await fetch('/api/comments', {
         method: 'POST',
         body: JSON.stringify({
-          postId,
+          //postId
           body
         }),
         headers: {
