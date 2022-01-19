0159260a746513fb93dbaf80cdce58945aea9678
Matt Reynolds <mattreynolds53@gmail.com>
2022-01-19 14:54:42 -0600

    Fixed comment.js files and replaced postId with new and correct blog_id.

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,28 +1,27 @@
-const commentFormHandler = async function(event) {
-    event.preventDefault();
- 
-  
-    //const postId = document.querySelector('input[name="post-id"]').value;
-    const body = document.querySelector('textarea[name="comment-body"]').value;
-    //console.log(postId);
-    console.log(body);
-  
-    if (body) {
-      await fetch('/api/comments', {
-        method: 'POST',
-        body: JSON.stringify({
-          //postId
-          body
-        }),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-  
-      document.location.reload();
-    }
-  };
-  
-  document
-    .querySelector('#new-comment-form')
-    .addEventListener('submit', commentFormHandler);
+const commentFormHandler = async function (event) {
+  event.preventDefault();
+
+  const blog_id = document.querySelector('input[name="post-id"]').value;
+  const body = document.querySelector('textarea[name="comment-body"]').value;
+  console.log(blog_id);
+  console.log(body);
+
+  if (body) {
+    await fetch('/api/comments', {
+      method: 'POST',
+      body: JSON.stringify({
+        blog_id,
+        body,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    document.location.reload();
+  }
+};
+
+document
+  .querySelector('#new-comment-form')
+  .addEventListener('submit', commentFormHandler);
